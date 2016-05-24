ad6c43ad1ff322cd5c68b292b0584ef536f06d2e
Xiaowei Liu <liu_xiaowei@outlook.com>
2016-05-24 11:04:43 +0800

    [update] update LambdaDemo.java

--- JavaBasic/LambdaDemo.java
+++ JavaBasic/LambdaDemo.java
@@ -1,7 +1,8 @@
-package ucas.lxw.java.lambda;
+//package ucas.lxw.java.lambda;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Comparator;
 
 /*
 Basic syntax:
@@ -22,11 +23,8 @@ public class LambdaDemo{
 	public static void main(String[] args){
 		String[] atp = {"Rafael Nadal",
 				"Novak Djokovic", 
-				"Stanislas Wawrinka",
-				"David Ferrer",
 				"Roger Federer",
 				"Andy Murray",
-				"Tomas Berdych",
 				"Juan Martin",
 				"Del Potro"
 				};
@@ -40,8 +38,11 @@ public class LambdaDemo{
 		
 		//Demo 1: lambda (Java 8)
 		players.forEach((player) -> System.out.print(player + "; "));
+		System.out.println();
 
 		//Demo 2: (Java 8)
+		//表达式 System.out::println 是一个方法引用，等同于 lambda 表达式 x -> System.out.println(x);
+		//Math::pow 等同于 (x, y) -> Math.pow(x, y);
 		players.forEach(System.out::println);
 
 		/*
@@ -56,8 +57,22 @@ public class LambdaDemo{
 		// 或者使用 lambda expression  
 		btn.setOnAction(event -> System.out.println("Hello World!"));  
 		*/
+
+		//Tips:
+		//如果 lambda 表达式的参数类型可以被推导，那么可以省略掉。
+		Comparator<String> comparator = (String first, String second) -> Integer.compare(first.length(), second.length());
+		comparator = (first, second) -> Integer.compare(first.length(), second.length());
+		//上面的例子当中会推导出 first 和 second 的类型是 String， 因为表达式赋值给了一个字符串比较器。
+
+		/*
+		//Notes
+		在 lambda 表达式当中不允许声明一个与局部变量同名的参数或者局部变量。 
+		String first = ""; 
+		Comparator<String> comparator = (first, second) -> Integer.compare(first.length(), second.length()); //编译会出错
+		*/
 	}
 }
 /*Reference:
 Java Lambda表达式入门: http://blog.csdn.net/renfufei/article/details/24600507
+Java 8 Lambda 表达式学习心得总结: http://www.codeceo.com/article/java8-lambda-usage.html#0-tsina-1-7723-397232819ff9a47a7b7e80a40613cfe1
 */
