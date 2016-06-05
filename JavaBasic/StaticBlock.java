ff9775b62550ce36bbd15d2e7bd636449b3f4595
Xiaowei Liu <liu_xiaowei@outlook.com>
2016-06-05 17:13:44 +0800

    [update] Create StaticBlock.java

---
+++ JavaBasic/StaticBlock.java
@@ -0,0 +1,13 @@
+public class StaticBlock{
+	{
+		//只有创建该类的对象时才会执行
+		System.out.println("2");
+	}
+	static {
+		System.out.println("1");
+	}
+	public static void main(String[] args){
+		Test test = new Test();
+		System.out.println("3");
+	}
+}
